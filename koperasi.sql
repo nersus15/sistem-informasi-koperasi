608b34fe4da40cb9ccda47b37af884f4db036d6f
nersus15 <38497343+nersus15@users.noreply.github.com>
2019-10-30 22:37:08 +0800

    Add files via upload
    add database/ Sql File (30/10/2019)

---
+++ koperasi.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2019 at 03:31 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `koperasi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id` varchar(25) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `NIK` varchar(12) NOT NULL,
+  `tanggal_lahir` varchar(25) NOT NULL,
+  `ktp` varchar(50) NOT NULL,
+  `account` varchar(50) NOT NULL,
+  `status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id`, `nama_lengkap`, `alamat`, `NIK`, `tanggal_lahir`, `ktp`, `account`, `status`) VALUES
+('5db85fa51d269', 'Fathurrahman', 'Jl. Arya Banjar Getas, Tj. Karang Permai, Sekarbela, Kota Mataram, Nusa Tenggara Barat', '14115151', '2019-10-29', 'hoho.PNG', 'fathur.pashter15@gmail.co', '1'),
+('5db8f398cd86f', 'Wiwin Mistiani', 'Lombok Timur', '151251511', '2019-10-08', 'IMG_7794.JPG', 'fathur.asher15@gmail.com', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` varchar(50) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `img` varchar(50) NOT NULL,
+  `status` int(1) NOT NULL,
+  `role` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `img`, `status`, `role`) VALUES
+('5ccd27fba8af7', 'fathur', 'fathur.ashter15@gmail.com', '$2y$10$rONYvHAqcnptthrIJztb6eYhYQcWf86iA3S2Z1sFxEiVph76Dn/xC', '5d97f51bc7295.png', 1, 1),
+('5db8f398cf457', '151251511', 'fathur.asher15@gmail.com', '$2y$10$MZHcSDZbMgmoazcIV7cJpOCtDiLAuFbik5gIsU6ss6RnSlYQ.7Y/y', 'default.jpg', 1, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
