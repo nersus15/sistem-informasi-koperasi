3a2ea698f51b078f999c88242b9873557ef9a92c
fathurrahman <fathur.ashter15@gmail.com>
2019-12-10 09:33:45 +0800

    add validator.js (helper)

---
+++ apps/helper/validator.js
@@ -0,0 +1,7 @@
+function numberValidation(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+
+        return false;
+    return true;
+}
